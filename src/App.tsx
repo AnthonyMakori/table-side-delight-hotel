c766aeca2cf3459401576f335542a73490c5f3e5
AnthonyMakori <antonymakori2019@gmail.com>
2025-07-09 13:19:23 +0300

    resolved import

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "../pages/Index";
 import NotFound from "../pages/NotFound";
-import  SignIn  from "../pages/auth/SignIn";
+import  SignIn  from "../pages/auth/signin";
 import Admin from "../pages/admin/dash";
 import QR from "../pages/QR/QRread";
 
@@ -19,7 +19,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/scan/Scan" element={<SignIn />} />       
           <Route path="/auth/signin" element={<SignIn />} /> 
           <Route path ="/admin/dash" element={<Admin />} />
           <Route path="/QR/QRread" element={<QR />} />
