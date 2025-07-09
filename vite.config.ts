3cb8cde55c6d8f539857f789e87a1eacab9ccbda
AnthonyMakori <antonymakori2019@gmail.com>
2025-07-09 13:59:17 +0300

    vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import react from "@vitejs/plugin-react";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
+      '@pages': path.resolve(__dirname, './pages'),
+
     },
   },
-});
+  build: {
+    outDir: 'dist',
+  },
+}));
